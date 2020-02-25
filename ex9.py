899d15742dfd4c3e7e62963eed581e55dacb1f5c
Nicole Beamer <tulip@Nicoles-iMac.hsd1.ut.comcast.net>
2020-02-25 11:37:56 -0700

    ex9

---
+++ ex9.py
@@ -0,0 +1,14 @@
+# here's some new strange stucc, remeber type it exactly
+
+days = "Mon Tue Wen Thu Fri Sat Sun"
+#this allows you to line break with \n
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nOct\nNov\nDec"
+
+print("Here are the days: ", days)
+print("here are the months: ", months)
+#this allows you to print on multiple lines with using a line break
+print("""there is something
+going on here with these three double-quotes.
+We'll be able to type as much as we like.
+even four lines!
+""")
