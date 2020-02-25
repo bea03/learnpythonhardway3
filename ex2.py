c4eec06423646f617d6cda0aec73128a8715c114
Nicole Beamer <tulip@Nicoles-iMac.hsd1.ut.comcast.net>
2020-02-25 08:59:03 -0700

    first commit

---
+++ ex2.py
@@ -0,0 +1,11 @@
+#A comment, this is so you can read your program later
+#Anything after the # is ignored by python.
+'''This should be a section of comments. Tres Commas.
+it spans multiple lines until python sees...
+TRES COMMAS
+'''
+
+print("I could have used code like this.") # and the comment is not seen
+
+## You can also use a comment to "disable" or comment out code:
+#print("this wont run")
